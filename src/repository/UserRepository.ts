c6cd5adb1b37afb515509d6dd9b61f315801e21e
alegandolfo <alegandolfo10@gmail.com>
2025-02-25 14:48:52 -0300

    chore: add sector dataypes

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,9 +1,10 @@
+import { SectorTypes } from "../model/SectorTypes"
 import { UserEntity } from "../model/UserEntity"
 import { IError } from "../utils/iError"
 
 export interface UserRepository {
-    createUser(email: string, name: string, password: string, sector: string): Promise<UserEntity|IError>
+    createUser(email: string, name: string, password: string, sector: SectorTypes): Promise<UserEntity|IError>
     getUser(email: string): Promise<UserEntity|IError>
-    updateUser(email: string, name?: string, password?: string, sector?: string): Promise<UserEntity|IError>
+    updateUser(email: string, name?: string, password?: string, sector?: SectorTypes): Promise<UserEntity|IError>
     deleteUser(email: string): Promise<boolean|IError>
 }
