090b4ac36b35588a22ad24f9f23fe768d0004cc7
alegandolfo <alegandolfo10@gmail.com>
2025-02-26 14:41:35 -0300

    fix: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import './utils/database'
 import express from 'express'
+import cors from 'cors'
 import { PORT } from './config.js'
 import userRoute from './routes/userRoute'
 import postRoute from './routes/postRoute'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.json({message: 'Bem-vindo a API.'})
-})
-
+app.use(cors())
 app.use('/user', userRoute)
 app.use('/post', postRoute)
+
+app.get('/', (req, res) => {
+    res.json({message: 'Bem-vindo a API Help-System.'})
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}.`)
